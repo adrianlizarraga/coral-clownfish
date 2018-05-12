e9dee33b3d6a4492f076885a6463bde09ca307dd
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2018-05-12 02:52:40 -0700

    Renamed

--- src/suggesters.js
+++ src/suggesters.js
@@ -1,7 +1,7 @@
 import RandomSuggester from './suggesters/random';
-import DumbSuggester from './suggesters/dumb';
+import BruteSuggester from './suggesters/brute';
 
 // Reference your suggester class here using an import statment like above.
 
 // Add a new instance of your suggester here. It will then show up in game. 
-export default [ new RandomSuggester('Scott\'s randomizer'), new DumbSuggester('Dumb thing') ];
+export default [ new RandomSuggester('Scott\'s randomizer'), new BruteSuggester('Adriaaaan') ];
